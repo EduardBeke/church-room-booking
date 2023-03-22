ee56c3c9f8c9b11cd12ef36a08652a939b97ba5a
Eduard <bekedarwin@gmail.com>
2023-03-22 11:05:25 +0100

    project init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+global using FastEndpoints;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddFastEndpoints();
+
+var app = builder.Build();
+app.UseAuthorization();
+app.UseFastEndpoints();
+app.Run();
+
+namespace MyNamespace
+{
+    public record Room(string name);
+}
+
+
+
